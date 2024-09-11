8573670a93169bab7fa6fc24c529d200d48f8aa5
Manuel Alvarez <manuel.alvarez@pegasi.io>
2024-09-11 14:12:18 -0500

    return breeds in find breeds controller

--- src/controllers/breeds.controller.ts
+++ src/controllers/breeds.controller.ts
@@ -2,14 +2,15 @@ import * as service from "../services/breeds/breeds.service";
 
 export const findBreeds = async (req: any, res: any) => {
   try {
-    const response = await service.findBreeds(req.query);
+    const response = await service.findBreeds(req.params);
     let data: any = await response.json();
     data = data.map((breed: any) => {
       return {
         _id: breed?.id,
         url: breed?.url,
-        name: breed.breeds[0]?.name,
-        breed_id: breed.breeds[0]?.id,
+        breeds: breed?.breeds[0],
+        name: breed?.breeds[0]?.name,
+        breed_id: breed?.breeds[0]?.id,
       };
     });
     res.json(data);
